5b46d9df4876ac7ba76ae91847128420cfc5d01e
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-09-22 01:46:00 +0900

    fixed: refactor

--- UIHoleView/UIHoleView.swift
+++ UIHoleView/UIHoleView.swift
@@ -1,19 +1,39 @@
 import UIKit
 
+public protocol DrawHole {
+    func draw(_ rect: CGRect, hole: Hole)
+}
+
+public class DrawSquareHole: DrawHole {
+    public func draw(_ rect: CGRect, hole: Hole) {
+        let holeIntersection = rect.intersection(hole.rect)
+        if holeIntersection.isNull { return }
+        hole.color.setFill()
+        UIRectFill(holeIntersection)
+    }
+}
+
+public class DrawRoundedCornerHole: DrawHole {
+    public func draw(_ rect: CGRect, hole: Hole) {}
+}
+
 public class Hole {
-    public var rect: CGRect!
-    public var color: UIColor!
-    public var cornerRadius: CGFloat?
+    internal var rect: CGRect!
+    internal var color: UIColor!
+    internal var cornerRadius: CGFloat?
+    internal let drawer: DrawHole!
     
     init(rect: CGRect, color: UIColor, cornerRadius: CGFloat) {
         self.rect   = rect
         self.color  = color
         self.cornerRadius = cornerRadius
+        self.drawer = DrawRoundedCornerHole()
     }
     
     init(rect: CGRect, color: UIColor) {
         self.rect  = rect
         self.color = color
+        self.drawer = DrawSquareHole()
     }
 }
 
@@ -36,22 +56,8 @@ public class UIHoleView: UIView {
         guard let holes = self.holes else { return }
         
         for hole in holes {
-            if let _ = hole.cornerRadius {
-                self.drawRoundedCornerHole(rect, hole)
-            } else {
-                self.drawSquareHole(rect, hole)
-            }
+            hole.drawer.draw(rect, hole: hole)
         }
-    }
-    
-    private func drawSquareHole(_ rect: CGRect, _ hole: Hole) {
-        let holeIntersection = rect.intersection(hole.rect)
-        if holeIntersection.isNull { return }
-        hole.color.setFill()
-        UIRectFill(holeIntersection)
-    }
-    
-    private func drawRoundedCornerHole(_ rect: CGRect, _ hole: Hole) {
     }
 }
 
